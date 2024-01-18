3e9e1d5e26a0b690f934b830cdccfe48abc70445
Alireza <alirezagoldoos@gmail.com>
2024-01-18 20:44:19 +0330

    Update: updating init.py

--- init.py
+++ init.py
@@ -1,2 +1,7 @@
+# This will provide a sample data in your database
 import os
 os.system('manage.py makemigrations')
+os.system('manage.py migrate')
+os.system('manage.py loaddata sample_data.json')
+print(' ------------------------\n  Succecfuly initialized \n ------------------------')
+
